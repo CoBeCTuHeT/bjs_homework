8b1cf8924c64bee77ef286c1e8082ddb30b63b4d
Valery Molvistov <valera.kandamolvistov@gmail.com>
2021-04-22 23:37:31 +0300

    -

--- 1.2-extended-syntax/task.js
+++ 1.2-extended-syntax/task.js
@@ -1,8 +1,7 @@
+'use strict'
 function getResult(a, b, c) {
-    'use strict'
     let discriminant;
     let discriminantRoot;
-    let error = 'Корней не сущетсвует';
     let x = [];
     discriminant = b ** 2 - 4 * a * c;
     discriminantRoot = Math.sqrt(discriminant)
@@ -11,51 +10,35 @@ function getResult(a, b, c) {
         return x;
     } else if (discriminant === 0) {
         x[0] = -b / 2 * a
-        return x;
     } else if (discriminant > 0) {
         x[0] = (-b + discriminantRoot) / (2 * a)
         x[1] = (-b - discriminantRoot) / (2 * a)
-        return x;
     }
+    return x;
 }
 
 function getAverageMark(marks) {
+    marks = marks.splice(0, 5);
     if (marks.length === 0) {
         return 0;
-    } else if (marks.length >= 5) {
-        let appraisal = marks.splice(0, 5);
-        for (let i = 0, summ = 0; i < appraisal.length; i++) {
-            summ = summ + appraisal[i];
-            if(i === (appraisal.length - 1)) {
-                return summ / appraisal.length;
-            }
-        }
-    } else if (marks.length < 5) {
-        console.log(marks)
-        for (let i = 0, summ = 0; i < marks.length; i++) {
-            summ = summ + marks[i];
-            if(i === (marks.length - 1)) {
-                return summ / marks.length;
-            }
+    }
+    for (let i = 0, summ = 0; i < marks.length; i++) {
+        summ = summ + marks[i];
+        if (i === (marks.length - 1)) {
+            return summ / marks.length;
         }
     }
 }
 
 function askDrink(name, dateOfBirthday) {
-    let birthday; 
-    let presentTime;
     let age;
     let forAdultMessage = `Не желаете ли олд-фэшн, ${name}?`;
     let noAdult = `Сожалею, ${name}, но я не могу вам продать алкоголь. Могу предложить вам замечательный клюквенный компот.`;
-
-    birthday = dateOfBirthday.getFullYear()
-    presentTime = new Date;
-    presentTime = presentTime.getFullYear()
     
-    age = presentTime - birthday;
-    if(age < 18) {
+    age = new Date().getFullYear() - dateOfBirthday.getFullYear();
+    if (age < 18) {
         return noAdult;
-    }else if(age >= 18) {
-       return forAdultMessage;
+    } else if (age >= 18) {
+        return forAdultMessage;
     }
 }
--- 1.3-data-types/task.js
+++ 1.3-data-types/task.js
@@ -1,6 +1,18 @@
+'use strict'
+
 function calculateTotalMortgage(percent, contribution, amount, date) {
-    // код для задачи №1 писать здесь
-    // return totalAmount;
+    let creditBody;
+    creditBody = amount - contribution;
+    let todayDate = new Date();
+    let months;
+    let time;
+    time = (date.getTime() - todayDate.getTime())
+    console.log(time)
+    months = (time / (1000 * 60 * 60 * 24 * 30) % 12)
+    console.log(months)
+    let part = percent / (1 / 12)
+    let monthlyPayment = creditBody * (part + part / (((1 + part) * months) - 1))
+    console.log(monthlyPayment)
 }
 
 function getGreeting(name) {
